4d499f7dd58b63140848b03d4d1ab948ff82247e
Darko <darko.pokoracki@adacta-fintech.com>
2025-01-04 17:02:17 +0100

    products implementation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
