10e4090a3ff70a55578b7b62c8eb08536b5cc6b1
Darko <darko.pokoracki@adacta-fintech.com>
2025-01-07 13:22:29 +0100

    logic for adding new product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,12 +6,55 @@ use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\ProductImage;
 
 class ProductController extends Controller
 {
-    public function index() {
-        $products = Product::get();
+    public function index()
+    {
+        $products = Product::with('category', 'brand', 'product_images')->get();
+        $brands = Brand::get();
+        $categories = Category::get();
 
-        return Inertia::render('Admin/Product/Index', ['products' => $products]);
+        return Inertia::render(
+            'Admin/Product/Index',
+            [
+                'products' => $products,
+                'brands' => $brands,
+                'categories' => $categories
+            ]
+        );
+    }
+
+    public function store(Request $request)
+    {
+
+        $product = new Product;
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $product->save();
+
+        // ovde ulazimo samo ako smo za proizvod uplodovali i slike.
+        if ($request->hasFile('product_images')) {
+            $productImages = $request->file('product_images');
+            foreach ($productImages as $image) {
+                // Generate a unique name for the image using timestamp and random string
+                $uniqueName = time() . '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
+                // Store the image in the public folder with the unique name
+                $image->move('product_images', $uniqueName);
+                // Create a new product image record with the product_id and unique name
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueName,
+                ]);
+            }
+        }
+        return redirect()->route('admin.products.index')->with('success', 'Product created successfully.');
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
@@ -15,12 +16,14 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .use(ElementPlus)
-            .use(VueSweetalert2)
-            .mount(el);
+        const app =  createApp({ render: () => h(App, props) })
+            app.use(plugin)
+            app.use(ZiggyVue, Ziggy)
+            app.use(ElementPlus)
+            app.use(VueSweetalert2)
+            window.Swal =  app.config.globalProperties.$swal
+
+            app.mount(el)
     },
     progress: {
         color: '#4B5563',
