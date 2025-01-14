a4b47a10e284ec3617421d3e8ddfe23759e80e2d
Darko <darko.pokoracki@adacta-fintech.com>
2025-01-14 21:27:46 +0100

    implmentation

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -58,4 +58,44 @@ class ProductController extends Controller
         }
         return redirect()->route('admin.products.index')->with('success', 'Uspešno ste dodali proizvod');
     }
+
+        //update 
+        public function update(Request $request, $id)
+        {
+            $product = Product::findOrFail($id);
+    
+            // dd($product);
+            $product->title = $request->title;
+            $product->price = $request->price;
+            $product->quantity = $request->quantity;
+            $product->description = $request->description;
+            $product->category_id = $request->category_id;
+            $product->brand_id = $request->brand_id;
+            // Check if product images were uploaded
+            if ($request->hasFile('product_images')) {
+                $productImages = $request->file('product_images');
+                // Loop through each uploaded image
+                foreach ($productImages as $image) {
+                    // Generate a unique name for the image using timestamp and random string
+                    $uniqueName = time() . '-' . Str::random(10) . '.' . $image->getClientOriginalExtension();
+    
+                    // Store the image in the public folder with the unique name
+                    $image->move('product_images', $uniqueName);
+    
+                    // Create a new product image record with the product_id and unique name
+                    ProductImage::create([
+                        'product_id' => $product->id,
+                        'image' => 'product_images/' . $uniqueName,
+                    ]);
+                }
+            }
+            $product->update();
+            return redirect()->route('admin.products.index')->with('success', 'Proizvod je uspešno izmenjen.');
+        }
+
+    public function deleteImage($id)
+    {
+        $image = ProductImage::where('id', $id)->delete();
+        return redirect()->route('admin.products.index')->with('success', 'Fotografija je uspešno obrisana.');
+    }
 }
