b87c8a8f3e126d7751eb431867ff0d73d20ee9e6
Darko <darko.pokoracki@adacta-fintech.com>
2025-01-15 16:59:23 +0100

    structure for further building

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,3 +1,14 @@
+<script setup>
+    // Import User Layout
+    import UserLayouts from './Layouts/UserLayouts.vue';
+
+
+    const products = ['seiko', 'casio', 'apple watch'];
+</script>
+
+
 <template>
-    Hello World
+    <UserLayouts>
+        <h2>Here is products list</h2>
+    </UserLayouts>
 </template>
