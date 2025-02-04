8f74ddc13148407fa331df532a135cf4da81d1ba
Darko <darko.pokoracki@adacta-fintech.com>
2025-02-04 23:05:04 +0100

    plus fixes

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -20,48 +20,17 @@
             <div
                 class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
             ></div>
-            <div class="grid grid-cols-2 gap-4 mb-4">
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-            </div>
-            <div
-                class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-            ></div>
-            <div class="grid grid-cols-2 gap-4">
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-                <div
-                    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-                ></div>
-            </div>
         </div>
     </AdminLayout>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
-import AdminLayout from './Components/AdminLayout.vue';
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+import AdminLayout from "./Components/AdminLayout.vue";
 
 // initialize components based on data attribute selectors
 onMounted(() => {
     initFlowbite();
-})
+});
 </script>
